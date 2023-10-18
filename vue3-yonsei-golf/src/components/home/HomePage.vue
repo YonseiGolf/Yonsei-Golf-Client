43be9b9ddfd96e363f7928d658c431c02d67baf0
birdieHyun <flaehdan@gmail.com>
2023-10-18 18:39:20 +0900

    feat: MainInfo 개발 (#7)

---
+++ vue3-yonsei-golf/src/components/home/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <MainInfo></MainInfo>
+    <ClubIntroduction></ClubIntroduction>
+  </div>
+</template>
+
+<script>
+import MainInfo from './MainInfo.vue'
+import ClubIntroduction from "@/components/home/ClubIntroduction.vue";
+
+export default {
+
+  components: {
+    MainInfo,
+    ClubIntroduction,
+  }
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue3-yonsei-golf/src/components/home/MainInfo.vue
+++ vue3-yonsei-golf/src/components/home/MainInfo.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-
+  <div class="background-image">
+    <div id="yonsei-golf">
+    YONSEI GOLF<br>
+    </div>
+    <div id="info">
+    연세 골프는 골프를 시작한지 얼마 되지 않은 초심자부터 프로까지<br>
+    다양한 실력을 보유하고 있는 사람들과 함께 합니다.
+    </div>
   </div>
 </template>
 
@@ -11,5 +17,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.background-image {
+  //width: 100%;
+  padding-top: (1284 / 946) * 10%;
+  position: relative;
+
+  background-image: url('../../img/main_info_image.jpg');
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  opacity: 0.8;
+}
+
+#yonsei-golf {
+  font-size: 100px;
+
+  color: white;
+}
+
+#info {
+  font-size: 25px;
+  color: white;
+}
 
 </style>
