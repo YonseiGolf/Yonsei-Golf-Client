d30a78d5caaad70ad0e430bc274bbd4e0eb44a45
birdieHyun <flaehdan@gmail.com>
2023-10-18 17:11:26 +0900

    feat: Header, Footer 추가

--- vue3-yonsei-golf/src/App.vue
+++ vue3-yonsei-golf/src/App.vue
@@ -1,20 +1,49 @@
 <template>
   <div id="app">
-    <Header></Header>
+    <header>
+      <CommonHeader></CommonHeader>
+    </header>
+
+    <div>내용</div>
+
+    <footer>
+      <CommonFooter></CommonFooter>
+    </footer>
+
+
   </div>
 </template>
 
 <script>
-import Header from './components/common/Header.vue'
+import CommonHeader from './components/common/CommonHeader.vue'
+import CommonFooter from './components/common/CommonFooter.vue'
 export default {
   name: 'App',
 
   components: {
-    Header,
+    CommonHeader,
+    CommonFooter,
   }
 }
 </script>
 
 <style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 
+  > common-footer {
+    position: sticky;
+    top: 0;
+    z-index: 1000;
+  }
+
+  > footer {
+    margin-top: auto;
+  }
+}
+body {
+  margin: 0;
+}
 </style>
