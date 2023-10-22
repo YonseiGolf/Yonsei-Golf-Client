7c5047cba77e8bed620ff0a1aae152d3cdfe51c9
임동현 <flaehdan@gmail.com>
2023-10-22 20:37:19 +0900

    feat: 지원 기간 가능 여부에 따른 다른 페이지 랜더링 (#30)

--- vue3-yonsei-golf/src/components/application/ApplicationPage.vue
+++ vue3-yonsei-golf/src/components/application/ApplicationPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <NotApplyTerm></NotApplyTerm>
-    <ApplyTerm></ApplyTerm>
+    <ApplyTerm v-if="applicationAvailable"></ApplyTerm>
+    <NotApplyTerm v-else></NotApplyTerm>
     <QnA></QnA>
   </div>
 </template>
@@ -10,13 +10,32 @@
 import NotApplyTerm from "@/components/application/NotApplyTerm.vue";
 import ApplyTerm from "@/components/application/ApplyTerm.vue";
 import QnA from "@/components/qna/QnA.vue";
+import axios from "axios";
 export default {
+
+  data() {
+    return {
+      applicationAvailable: false,
+    }
+  },
 
   components: {
     ApplyTerm,
     NotApplyTerm,
     QnA,
   },
+
+  async mounted() {
+    try {
+      const response = await axios.get(`${process.env.VUE_APP_LOCAL_API_URL}/application/availability`);
+
+      if (response.status === 200) {
+        this.applicationAvailable = response.data.data;
+      }
+    }catch (error){
+      console.error('API 호출 중 오류 발생:', error);
+    }
+  }
 }
 </script>
 
