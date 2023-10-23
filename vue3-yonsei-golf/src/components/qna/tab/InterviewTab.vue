6d30a70be8633829c4b6950907897e89f990ada4
임동현 <flaehdan@gmail.com>
2023-10-23 18:52:54 +0900

    Design/#35 center align (#36)
    * feat: qna 화면 중앙 정렬
    
    * refactor: 사용하지 않는 em 태그 삭제
    
    * feat: 동아리 활동 소개 중앙 정렬

--- vue3-yonsei-golf/src/components/qna/tab/InterviewTab.vue
+++ vue3-yonsei-golf/src/components/qna/tab/InterviewTab.vue
@@ -1,45 +1,58 @@
 <template>
   <!--    면접-->
-  <input type="radio" name="accordion" id="interview-answer01">
-  <label for="interview-answer01">
-    질문글 1 <em></em>
-  </label>
-  <div>
-    <p>
-      답변글 1
-    </p>
+  <div class="answer-container">
+    <input type="checkbox" id="interview-answer01">
+    <label for="interview-answer01">
+      질문글 1
+    </label>
+    <div>
+      <p>
+        답변글 1
+      </p>
+    </div>
   </div>
 
-  <input type="radio" name="accordion" id="interview-answer02">
-  <label for="interview-answer02">
-    질문글 2 <em></em>
-  </label>
-  <div>
-    <p>
-      답변글 2
-    </p>
+  <div class="answer-container">
+    <input type="checkbox" id="interview-answer02">
+    <label for="interview-answer02">
+      질문글 2
+    </label>
+    <div>
+      <p>
+        답변글 2
+      </p>
+    </div>
   </div>
 
-  <input type="radio" name="accordion" id="interview-answer03">
-  <label for="interview-answer03">
-    질문글 3 <em></em>
-  </label>
-  <div>
-    <p>
-      답변글 3
-    </p>
+  <div class="answer-container">
+    <input type="checkbox" id="interview-answer03">
+    <label for="interview-answer03">
+      질문글 3
+    </label>
+    <div>
+      <p>
+        답변글 3
+      </p>
+    </div>
+
   </div>
-
-
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
+
+.answer-container {
+  margin: 0 auto;
+  border: 1px solid #f2f2f2;
+  border-radius: 20px;
+  width: 800px;
+  margin-bottom: 20px;
+  text-align: left;
+  background: #f2f2f2;
+}
 
 input[id*="answer"] {
   display: none;
@@ -48,7 +61,6 @@ input[id*="answer"] {
 input[id*="answer"] + label {
   display: block;
   padding: 20px;
-  border: 1px solid #143790;
   border-bottom: 0;
   color: black;
   font-weight: 900;
@@ -56,23 +68,13 @@ input[id*="answer"] + label {
   position: relative;
 }
 
-input[id*="answer"] + label em {
-  position: absolute; // 오른쪽에 자율 배치
-  top: 50%;
-  right: 10px;
-  width: 30px;
-  height: 30px;
-  margin-top: -15px;
-  display: inline-block;
-  background: url('../../../img/instalogo.png') 0 0 no-repeat
-}
-
 input[id*="answer"] + label + div {
   max-height: 0;
   transition: all .35s;
   overflow: hidden; // 안의 내용이 삐져나오지 않음
-  background: #ebf8ff;
-  font-size: 11px;
+  font-size: 14px;
+  font-weight: bold;
+  margin-left: 20px;
 }
 
 input[id*="answer"] + label + p {
@@ -84,7 +86,4 @@ input[id*="answer"]:checked + label + div {
   max-height: 100px;
 }
 
-input[id*="answer"]:checked + label em {
-  background-position: 0 -30px;
-}
 </style>
--- vue3-yonsei-golf/src/components/qna/tab/QualificationsTab.vue
+++ vue3-yonsei-golf/src/components/qna/tab/QualificationsTab.vue
@@ -1,35 +1,41 @@
 <template>
-    <!--    지원 자격-->
-    <input type="radio" name="accordion" id="qualifications-answer01">
+  <!--    지원 자격-->
+  <div class="answer-container">
+    <input type="checkbox" id="qualifications-answer01">
     <label for="qualifications-answer01">
-      질문글 1 <em></em>
+      Q: 연세 골프의 지원 자격
     </label>
     <div>
       <p>
-        답변글 1
+        A: 골프에 관심있는 모든 연세인
       </p>
     </div>
+  </div>
 
-    <input type="radio" name="accordion" id="qualifications-answer02">
+  <div class="answer-container">
+    <input type="checkbox" id="qualifications-answer02">
     <label for="qualifications-answer02">
-      질문글 2 <em></em>
+      질문글 2
     </label>
     <div>
       <p>
         답변글 2
       </p>
     </div>
+  </div>
 
-<!--  type="checkbox"로 (name 없이)하면 탭이 열린채로 둘 수 있고, type=radio, name=accordion 으로 하면, 탭은 하나만 열리도록 할 수 있음-->
-    <input type="radio" name="accordion" id="qualifications-answer03">
+  <!--  type="checkbox"로 (name 없이)하면 탭이 열린채로 둘 수 있고, type=radio, name=accordion 으로 하면, 탭은 하나만 열리도록 할 수 있음-->
+  <div class="answer-container">
+    <input type="checkbox" id="qualifications-answer03">
     <label for="qualifications-answer03">
-      질문글 3 <em></em>
+      질문글 3
     </label>
     <div>
       <p>
         답변글 3
       </p>
     </div>
+  </div>
 </template>
 
 <script>
@@ -38,6 +44,16 @@ export default {}
 
 <style lang="scss" scoped>
 
+.answer-container {
+  margin: 0 auto;
+  border: 1px solid #f2f2f2;
+  border-radius: 20px;
+  width: 800px;
+  margin-bottom: 20px;
+  text-align: left;
+  background: #f2f2f2;
+}
+
 input[id*="answer"] {
   display: none;
 }
@@ -45,7 +61,7 @@ input[id*="answer"] {
 input[id*="answer"] + label {
   display: block;
   padding: 20px;
-  border: 1px solid #143790;
+  //border: 1px solid #143790;
   border-bottom: 0;
   color: black;
   font-weight: 900;
@@ -53,35 +69,22 @@ input[id*="answer"] + label {
   position: relative;
 }
 
-input[id*="answer"] + label em {
-  position: absolute; // 오른쪽에 자율 배치
-  top: 50%;
-  right: 10px;
-  width: 30px;
-  height: 30px;
-  margin-top: -15px;
-  display: inline-block;
-  background: url('../../../img/instalogo.png') 0 0 no-repeat
-}
-
 input[id*="answer"] + label + div {
   max-height: 0;
   transition: all .35s;
   overflow: hidden; // 안의 내용이 삐져나오지 않음
-  background: #ebf8ff;
-  font-size: 11px;
+  font-size: 14px;
+  font-weight: bold;
+  margin-left: 20px;
 }
 
 input[id*="answer"] + label + p {
   display: inline-block;
+  overflow: hidden;
   padding: 20px;
 }
 
 input[id*="answer"]:checked + label + div {
   max-height: 100px;
-}
-
-input[id*="answer"]:checked + label em {
-  background-position: 0 -30px;
 }
 </style>
