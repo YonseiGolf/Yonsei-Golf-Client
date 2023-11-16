0309ee66753c5baf084a787770707a34b3cb75ef
birdie <flaehdan@gmail.com>
2023-11-16 21:47:31 +0900

    feat: 모바일 햄버거 추가

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -7,8 +7,6 @@
     </div>
     <nav>
       <ul class="header-nav">
-
-
         <li>
           <router-link to="/">Home</router-link>
         </li>
@@ -51,6 +49,65 @@
         <li v-else class="logout-button" @click="logout">로그아웃</li>
       </ul>
     </nav>
+
+    <div class="mobile_btn">
+      <input type="checkbox" id="hamburger" v-model="isHamburgerOpen"/>
+      <label for="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+
+
+
+
+      <div class="sidebar">
+        <img src="https://yg-img-storage.s3.ap-northeast-2.amazonaws.com/image/logo.5d322a8d.png" alt="Logo" width=100/>
+        <hr style="position: relative; top:100px; border: solid 1px black;">
+        <ul class="nav_mobile">
+          <li>
+            <router-link to="/" @click="closeHamburger">Home</router-link>
+          </li>
+
+          <li v-if="adminStatus">
+            <router-link to="/admin/apply-alarm" @click="closeHamburger">
+              지원 대기
+            </router-link>
+          </li>
+
+          <li v-if="adminStatus">
+            <router-link to="/admin/form" @click="closeHamburger">
+              지원서 관리
+            </router-link>
+          </li>
+
+          <li v-if="adminStatus">
+            <router-link to="/admin/users" @click="closeHamburger">
+              회원 관리
+            </router-link>
+          </li>
+
+          <li v-if="adminStatus">
+            <router-link to="/apply/form" @click="closeHamburger">지원서</router-link>
+          </li>
+
+          <li>
+            <router-link to="/board" @click="closeHamburger">게시판</router-link>
+          </li>
+          <li>
+            <router-link to="/recruit" @click="closeHamburger">모집안내</router-link>
+          </li>
+          <li>
+            <router-link to="/apply" @click="closeHamburger">지원하기</router-link>
+          </li>
+          <li v-if="!username">
+            <router-link to="/login" @click="closeHamburger">로그인</router-link>
+          </li>
+          <li v-else class="logout-button" @click="logout">로그아웃</li>
+        </ul>
+      </div>
+    </div>
+
   </header>
 </template>
 
@@ -79,6 +136,23 @@ export default {
     return {
       username, adminStatus, logout
     };
+  },
+
+  data() {
+    return {
+      isHamburgerOpen: false, // 햄버거 메뉴의 상태를 저장
+    }
+  },
+
+  methods: {
+    // 햄버거 메뉴를 토글하는 메소드
+    toggleHamburger() {
+      this.isHamburgerOpen = !this.isHamburgerOpen;
+    },
+    // 햄버거 메뉴를 닫는 메소드
+    closeHamburger() {
+      this.isHamburgerOpen = false;
+    },
   },
 
 }
@@ -132,6 +206,185 @@ nav a:hover {
 
 .logout-button:hover {
   color: black; /* 호버 시 텍스트 색상 변경 */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+a {text-decoration: none;
+  color: black;}
+
+html,body {width: 100%; overflow-x: hidden; overflow-y: auto;}
+
+* {margin: 0; padding: 0;}
+
+.mobile_btn {
+  display: none; /* 기본적으로 숨깁니다 */
+  position: absolute;
+  //top: 70px;
+  right: 15px; /* 왼쪽 대신 오른쪽 정렬 */
+  z-index: 100; /* 다른 요소 위에 표시되도록 z-index 설정 */
+}
+
+input[id="hamburger"] {
+  display: none;
+}
+input[id="hamburger"] + label {
+  display: block;
+  width: 60px;
+  height: 40px;
+  position: relative;
+  cursor: pointer;
+}
+input[id="hamburger"] + label span {
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 5px;
+  border-radius: 30px;
+  background: #000;
+  transition: all 0.35s;
+}
+input[id="hamburger"] + label span:nth-child(1) {
+  top: 0;
+}
+input[id="hamburger"] + label span:nth-child(2) {
+  top: 50%;
+  transform: translateY(-50%);
+}
+input[id="hamburger"] + label span:nth-child(3) {
+  bottom: 0;
+}
+input[id="hamburger"]:checked + label {
+  z-index: 99;
+}
+input[id="hamburger"]:checked + label span {
+  background: black;
+}
+input[id="hamburger"]:checked + label span:nth-child(1) {
+  top: 50%;
+  transform: translateY(-50%) rotate(45deg);
+}
+input[id="hamburger"]:checked + label span:nth-child(2) {
+  opacity: 0;
+}
+input[id="hamburger"]:checked + label span:nth-child(3) {
+  bottom: 50%;
+  transform: translateY(50%) rotate(-45deg);
+}
+div[class="sidebar"] {
+  width: 100%;
+  height: 100%;
+  background: white;
+  position: fixed;
+  top: 0;
+  left: -100%;
+  z-index: 98;
+  transition: all 0.35s;
+}
+
+input[id="hamburger"]:checked + label + div {
+  left: 0;
+}
+
+.nav_mobile {
+  position: absolute;
+  top:119px;
+  width: 100%;
+}
+
+.sidebar .nav_mobile li {
+  color:black;
+  position:relative;
+  float:left;
+  width:100%;
+  list-style-type:none;
+  font-size:20px;
+  border-bottom:1px solid #ccc;
+  font-family: 'Noto Sans KR';
+  font-weight:bold;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  text-align: center;
+}
+
+.sidebar a {
+  padding: 0;
+  text-decoration: none;
+  display: block;
+}
+
+.sidebar li a{
+  display:block;
+  font-weight:bold;
+  line-height:40px;
+  margin:0px;
+  padding:0;
+  text-align:center;
+  text-decoration:none;
+  transition: all 0.4s;
+}
+
+.sidebar li a:hover, .sidebar ul li:hover a {
+
+  text-decoration: none;
+  color: #eee;
+  padding: 0;
+  margin: 0;
+  border: 0px;
+}
+
+//미디어 쿼리
+@media (min-width: 1024px) {
+  .mobile_btn, .sidebar {
+    display: none;
+  }
+
+  .header-nav {
+    display: flex;
+  }
+}
+
+
+@media (max-width: 1024px) {
+  .header-nav {
+    display: none;
+  }
+
+  .mobile_btn, .sidebar {
+    display: block;
+  }
+
+  .mobile_btn {
+    position: relative; /* 위치 조정이 필요하면 수정하세요. */
+  }
+}
+
+/* 햄버거 메뉴 버튼 크기 조정 */
+input[id="hamburger"] + label {
+  width: 50px; /* 너비를 줄입니다 */
+  height: 30px; /* 높이를 줄입니다 */
+}
+
+/* 햄버거 메뉴 선의 크기 조정 */
+input[id="hamburger"] + label span {
+  height: 3px; /* 선의 두께를 줄입니다 */
+  background: white;
+  /* 각 선의 위치 조정이 필요하다면 여기에서 조정합니다. */
+}
+
+/* 햄버거 메뉴 변형 시 선의 크기 조정 */
+input[id="hamburger"]:checked + label span:nth-child(1),
+input[id="hamburger"]:checked + label span:nth-child(3) {
+  /* 선의 두께가 변형되는 경우 여기에서 조정합니다. */
 }
 
 </style>
